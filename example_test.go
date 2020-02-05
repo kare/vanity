0eb21db4a080a45d778e4a92b5d6b5b55f74b0c5
Kare Nuorteva <kare.nuorteva@me.com>
2020-02-05 17:50:25 +0200

    Update example for IndexPage()

--- example_test.go
+++ example_test.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"kkn.fi/vanity"
 )
 
 func ExampleHandler() {
 	errorLog := log.New(os.Stderr, "vanity: ", log.Ldate|log.Ltime|log.LUTC)
+	indexPage := strings.NewReader("<html><h1>Vanity</h1></html>")
 	srv := vanity.Handler(
 		vanity.ModuleServerURL("https://pkg.go.dev"),
 		vanity.SetLogger(errorLog),
 		vanity.VCSURL("https://github.com/kare"),
 		vanity.VCS("git"),
+		vanity.IndexPage(indexPage),
 	)
 	http.Handle("/", srv)
 	// Output:
